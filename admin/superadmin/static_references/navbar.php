d06c1026ffce64960b345c47dea9f0747d33c5d7
Pritom Borogoria <pritom.saan@gmail.com>
2018-05-18 05:01:37 +0600

    First Customized Admin Theme

---
+++ admin/superadmin/static_references/navbar.php
@@ -0,0 +1,211 @@
+
+<!-- Top Bar -->
+<nav class="navbar">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
+            <a href="javascript:void(0);" class="bars"></a>
+            <a class="navbar-brand" href="index.html">Power Line Bangladesh</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Call Search -->
+                <li><a href="javascript:void(0);" class="js-search" data-close="true"><i class="material-icons">search</i></a></li>
+                <!-- #END# Call Search -->
+                <!-- Notifications -->
+                <li class="dropdown">
+                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                        <i class="material-icons">notifications</i>
+                        <span class="label-count">1</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="header">NOTIFICATIONS</li>
+                        <li class="body">
+                            <ul class="menu">
+                                <li>
+                                    <a href="javascript:void(0);">
+                                        <div class="icon-circle bg-light-green">
+                                            <i class="material-icons">person_add</i>
+                                        </div>
+                                        <div class="menu-info">
+                                            <h4>12 new members joined</h4>
+                                            <p>
+                                                <i class="material-icons">access_time</i> 14 mins ago
+                                            </p>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="footer">
+                            <a href="javascript:void(0);">View All Notifications</a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- #END# Notifications -->
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- #Top Bar -->
+<section>
+    <!-- Left Sidebar -->
+    <aside id="leftsidebar" class="sidebar">
+        <!-- User Info -->
+        <div class="user-info">
+            <div class="image">
+                <img src="../../static/images/user.png" width="48" height="48" alt="User" />
+            </div>
+            <div class="info-container">
+                <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">John Doe</div>
+                <div class="email">Superuser Panel</div>
+                <div class="btn-group user-helper-dropdown">
+                    <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
+                    <ul class="dropdown-menu pull-right">
+                        <li><a href="javascript:void(0);"><i class="material-icons">person</i>Profile</a></li>
+                        <li role="seperator" class="divider"></li>
+                        <li><a href="javascript:void(0);"><i class="material-icons">input</i>Sign Out</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- #User Info -->
+        <!-- Menu -->
+        <div class="menu">
+            <ul class="list">
+                <li class="header">MAIN NAVIGATION</li>
+                <li class="active">
+                    <a href="../superadmin/dashboard">
+                        <i class="material-icons">home</i>
+                        <span>Home</span>
+                    </a>
+                </li>
+
+
+                <li>
+                    <a href="javascript:void(0);" class="menu-toggle">
+                        <i class="material-icons">flight</i>
+                        <span>Shipments</span>
+                    </a>
+                    <ul class="ml-menu">
+                      <li>
+                          <a href="javascript:void(0);" >
+                              <i class="material-icons">view_stream</i>
+                              <span>Shippers List</span>
+                          </a>
+                      </li>
+                        <li>
+                            <a href="javascript:void(0);" >
+                                <i class="material-icons">create</i>
+                                <span>Create Shippers</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="../superadmin/dashboard">
+                        <i class="material-icons">note</i>
+                        <span>Manifest Report</span>
+                    </a>
+                </li>
+
+
+                <li>
+                    <a href="javascript:void(0);" class="menu-toggle">
+                        <i class="material-icons">local_shipping</i>
+                        <span>Shippers</span>
+                    </a>
+                    <ul class="ml-menu">
+                      <li>
+                          <a href="javascript:void(0);" >
+                              <i class="material-icons">view_stream</i>
+                              <span>Shippers List</span>
+                          </a>
+                      </li>
+                        <li>
+                            <a href="javascript:void(0);" >
+                                <i class="material-icons">create</i>
+                                <span>Create Shippers</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript:void(0);" class="menu-toggle">
+                        <i class="material-icons">shopping_cart</i>
+                        <span>Consignee</span>
+                    </a>
+                    <ul class="ml-menu">
+                      <li>
+                          <a href="javascript:void(0);" >
+                              <i class="material-icons">view_stream</i>
+                              <span>Consignee List</span>
+                          </a>
+                      </li>
+                        <li>
+                            <a href="javascript:void(0);" >
+                                <i class="material-icons">create</i>
+                                <span>Create Consignee</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript:void(0);" class="menu-toggle">
+                        <i class="material-icons">work</i>
+                        <span>Office Management</span>
+                    </a>
+                    <ul class="ml-menu">
+                      <li>
+                          <a href="javascript:void(0);" >
+                              <i class="material-icons">business</i>
+                              <span>Branch List</span>
+                          </a>
+                      </li>
+                        <li>
+                            <a href="javascript:void(0);" >
+                                <i class="material-icons">person</i>
+                                <span>User List</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" >
+                                <i class="material-icons">create</i>
+                                <span>Create Branch</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" >
+                                <i class="material-icons">person_add</i>
+                                <span>Create User</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="../superadmin/dashboard">
+                        <i class="material-icons">report</i>
+                        <span>System Log</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <!-- #Menu -->
+        <!-- Footer -->
+        <div class="legal">
+            <div class="copyright">
+                &copy; 2018 <a href="javascript:void(0);">Skooby - Technology for Impact</a>.
+            </div>
+            <div class="version">
+                <b>Version: </b> 1.0.1
+            </div>
+        </div>
+        <!-- #Footer -->
+    </aside>
+    <!-- #END# Left Sidebar -->
+</section>
